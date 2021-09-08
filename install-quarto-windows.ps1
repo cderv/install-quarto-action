bdb6a09e25decd7738eff37102ebbe34b25d210c
christophe dervieux <christophe.dervieux@gmail.com>
2021-09-08 18:58:05 +0200

    Don't use an alias

--- install-quarto-windows.ps1
+++ install-quarto-windows.ps1
@@ -1,4 +1,4 @@
-iex (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
+Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
 Join-Path (Resolve-Path ~).Path "scoop\shims" >> $Env:GITHUB_PATH
 scoop bucket add r-bucket https://github.com/cderv/r-bucket.git
 scoop install quarto
